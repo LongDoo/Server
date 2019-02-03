6df65a2d14a344f04526eb361712a8bc429ff015
Ryan-Willpower <surasak.c.work@gmail.com>
2019-02-03 23:35:39 +0700

    [update] rewrite code

--- routes/create.js
+++ routes/create.js
@@ -1,21 +1,34 @@
 module.exports = async (f, opt, next) => {
-  f.post('/post', async (req, res) => {
-    try {
-      const {title, excerpt, content, photo} = req.body
-      if (photo == undefined) {
-        const photo = null
-      }
-      const data = await f.db.execute(`
-        INSERT INTO Posts(title, excerpt, content, photo)
-        VALUE(?, ?, ?, ?)`, [title, excerpt, content, photo])
-      return {
-        message: 'add data success',
-        status: data
-      }
-    } catch (err) {
-      res.code(400)
-      return {
-        error: err
+  f.route({
+    method: 'POST',
+    url: '/create',
+    schema: {
+      querystring: {
+        title: { type: 'string' },
+        excerpt: { type: 'string' },
+        content: { type: 'string' },
+        photo: { type: 'string' },
+      },
+    },
+    handler: async (req, res) => {
+      try {
+        const {title, excerpt, content, photo} = req.body
+        const data = await f.db.execute(`
+          INSERT INTO Posts(title, excerpt, content, photo)
+          VALUE(?, ?, ?, ?)`, [title, excerpt, content, photo])
+        if (data) {
+          return {
+            message: 'add data success',
+            status: data
+          }
+        } else {
+          throw err
+        }
+      } catch (err) {
+        res.code(400)
+        return {
+          error: err
+        }
       }
     }
   })
--- routes/read_single.js
+++ routes/read_single.js
@@ -1,14 +1,30 @@
 module.exports = async (f, opt, next) => {
-  f.get('/post/id/:id', async (req, res) => {
-    try {
-      const [rows, fields] = await f.db.execute('SELECT * FROM Posts WHERE id=?', [req.params.id])
-      if (rows.length == 0) throw err
-      return rows
-    } catch (err) {
-      res.code(404)
-      res.send({
-        Error: 'id not found.'
-      })
+  f.route({
+    method: 'GET',
+    url: '/post/:id',
+    schema: {
+      querystring: {
+        id: { type: 'number' }
+      },
+      response: {
+        404: {
+          type: 'object',
+          properties: {
+            Error: { type: 'string' }
+          }
+        }
+      }
+    },
+    handler: async (req, res) => {
+      try {
+        const [rows, fields] = await f.db.execute('SELECT * FROM Posts WHERE id=?', [req.params.id])
+        return rows[0]
+      } catch (err) {
+        res.code(404)
+        return {
+          Error: 'id not found.'
+        }
+      }
     }
   })
 }
